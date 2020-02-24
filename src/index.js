677a27c044b76f9cdb120258e3ad55c6798d3c35
Dmitry Pashkevich <dmitry-pashkevich-by@mail.ru>
2020-02-25 00:06:56 +0300

    end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (n) {
+    
+    let matrix = [
+        ['','one hundred', 'two hundred','three hundred','four hundred','five hundred','six hundred','seven hundred','eight hundred','nine hundred'],
+        ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'],
+        ['','one','two','three','four','five','six','seven','eight', 'nine'],
+        ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+    ]
+
+    if( n == 0) return 'zero';
+    n = String(n).split('');
+    if(n.length == 2) n.unshift('0');
+    if(n.length == 1) return matrix[2][n[0]];
+
+    let newArr = [];
+
+
+    for(let i =0; i < n.length; i++) {
+        if(i == 1 && n[i] == 1) {
+            newArr.push(matrix[3][n[2]]);
+            break;
+        }
+
+        newArr.push(matrix[i][n[i]]);
+
+    }
+
+
+   return newArr.filter(el => el !== '').join(' ');
+   
+    
+
+
 }
+
+
